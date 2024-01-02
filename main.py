91d7e7fd5fa18409c9ac318f9c60a83507c85c47
Hwane0716 <155374859+Hwane0716@users.noreply.github.com>
2024-01-02 22:30:02 +0900

    Update main.py
    프롬 최적화

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ add_bg_from_url()
 image_url = 'https://i.imgur.com/xQIymoW.png'
 
 # 미리 정의한 질문 리스트
-questions = ["내 점집에 온거가 반갑다는 뜻 무슨 운세보러 왔수?", "나이가?", "성별이?", "좋아하는 색?"]
+questions = ["무슨 확률을 보러왔나요?[예를 들어 길가다 넘어질 확률]", "나이는?", "성별은?", "좋아하는 색?"]
 
 # 답변을 저장할 변수 초기화
 answer1 = ""
@@ -57,7 +57,7 @@ def get_answer(index):
 
 # 질문과 이미지를 함께 출력하고 답변을 입력받는 반복문
 for i, question in enumerate(questions):
-    st.image(image_url, caption=question, width=300)  # 이미지와 질문을 함께 띄움
+    st.image(image_url, caption=question, width=500)  # 이미지와 질문을 함께 띄움
     get_answer(i)  # 답변 받아서 저장
 
 # 답변 확인
@@ -70,7 +70,8 @@ st.title('점을.. 한번.. 봐볼까...')
 
 if st.button('Say hello'):
     result = chat_model.predict(
-    f"너는 이제 운세 전문가역할을 연기할거야 나는 {answer1}이런 운세를 보러 왔고 운세와 관련된 내용이 없으면 상관없는 내용이라고 출력해 나의 나이는 {answer2}이고 내 성별은 {answer3}이고 좋아하는 색은 {answer4}야 나의 나이와 성별 좋아하는 색을 종합해서 운을 봐줘 마지막으로 너는 고양이 운세전문가 역할이라서 말 마디끝마다 냥을 붙여야해", 
+    f"너는 이제부터 이상한 운세를 봐주는 역할이야 이상한 운세라 함은 길가다 넘어질 확률이나 길가다 돈을 주울 확률 길가다 연예인을 볼 확률같은걸 운세를 봐주면 돼 그리고 그 운세에는 타당한 이유가 있어야 하고 주어진 조건에 최대한 맞춰서 알아봐줘야해
+    내가 알아보고 싶은 것은{answer1}이고 , 나이는 {answer2}이고 , 성별은 {answer3}이고 , 좋아하는색은 {answer4}야 ", 
     temperature=0
     )
     st.write(result)
